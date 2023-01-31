c1c46e652fd9e45e575cf5776b35ced613834fe2
cedsalvet <cedric.salvetat@gmail.com>
2023-01-31 16:57:23 +0100

    added onmouseover function to draw a black pixel; added a slider to change the grid size

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
-let gridSize = 16
-let sketchGridDiv = document.querySelector("#sketchGrid");
+let gridSize = 16;
+
+drawGrid();
 
 
-for (var i=0; i<gridSize; i++) {
-    for (var j=0; j<gridSize; j++) {
-        let div = document.createElement("div");
-        div.setAttribute("id", `div${i}-${j}`)
-        div.setAttribute("class", "pixel");
-
-        sketchGridDiv.appendChild(div);
+function drawGrid() {
+    let sketchGridDiv = document.querySelector("#sketchGrid");
+    sketchGridDiv.replaceChildren();
+    for (var i=1; i<=gridSize; i++) {
+        for (var j=1; j<=gridSize; j++) {
+            let div = document.createElement("div");
+            div.setAttribute("id", `div${i}-${j}`)
+            div.setAttribute("class", "pixel");
+            div.addEventListener('mouseover', event => {toggleActive(event)});
+            sketchGridDiv.appendChild(div);
+        }
     }
+}
+
+function toggleActive(e){
+    if (e.buttons==1) {
+        e.target.style.transition = "all 0.1s"; 
+        e.target.classList.toggle("activePixel");
+    }
+}
+
+function gridChange(){
+    let slider = document.getElementById("sizeRange");
+    sketchGrid = document.getElementById("sketchGrid");
+    gridSize=slider.value;
+    sketchGrid.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+    sketchGrid.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+    drawGrid();
 }
